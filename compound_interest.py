57c80a425fc638cb475e8cd4731b2405c45f789a
Daniel Clavijo Núñez <148864537+Daniel0611CN@users.noreply.github.com>
2024-09-23 11:14:08 +0200

    Compound interest

--- compound_interest.py
+++ compound_interest.py
@@ -1,2 +1,13 @@
 # Aquí hacer el mismo ejercicio que el de interés compuesto en java Big Decimal
 
+from decimal import Decimal;
+
+aux = Decimal('10000.00')
+aux2 = Decimal('0.05')
+
+years = 10
+timesCompounded = 4
+
+# Aquí falta poner la fórmula.
+
+print(f"{years}")
